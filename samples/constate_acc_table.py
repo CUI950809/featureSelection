587b01f90383d0000b3fd2207cec7499aa69a6b0
gavin <243864952@qq.com>
2016-10-21 11:20:44 +0800

    修改了constate_acc_table.py，添加了部分代码，对每个算法的多条曲线进行了argmax并concat到一个表格中

--- samples/constate_acc_table.py
+++ samples/constate_acc_table.py
@@ -2,12 +2,13 @@ from samples.conf import pd
 from samples.conf import get_filepath_in_folders
 from samples.conf import *
 
-def constate_acc_table(path_list, fn):
-    for folder_path in path_list:
+def constate_acc_table(in_path_list, out_path_list, fn):
+    for fpidx, folder_path in enumerate(path_list):
         acc_path_list = get_filepath_in_folders(folder_path)
 
         print(folder_path)
 
+        max_acc_table = pd.DataFrame()
         acc_table = pd.DataFrame()
         for path in acc_path_list:
             if 'baseline' in path or fn in path:
@@ -16,6 +17,10 @@ def constate_acc_table(path_list, fn):
             table = pd.read_csv(path, index_col='index name')
             acc_table = pd.concat([acc_table, table])
             acc_table = pd.DataFrame(acc_table)
+
+            idx = table.apply(np.sum, axis=1).argmax()
+            max_acc_table = pd.concat((max_acc_table, pd.DataFrame(table.ix[idx, :]).T));
+            max_acc_table = pd.DataFrame(max_acc_table)
 
         for path in acc_path_list:
             if 'baseline' not in path or fn in path:
@@ -28,9 +33,13 @@ def constate_acc_table(path_list, fn):
             baseline_table = pd.DataFrame(baseline.repeat(num_col).reshape(1,num_col),
                                           columns=columns, index=index_name)
             baseline_table.index.names = acc_table.index.names
-            acc_table = pd.concat((baseline_table, acc_table))
 
-        acc_table.to_csv(folder_path + '/' + fn, header = True, index = True)
+            acc_table = pd.concat((baseline_table, acc_table))
+            max_acc_table = pd.concat((baseline_table, max_acc_table))
+
+        create_path(out_path_list[fpidx])
+        acc_table.to_csv(out_path_list[fpidx] + '/' + fn + '.csv', header = True, index = True)
+        max_acc_table.to_csv(out_path_list[fpidx] + '/' + 'max_' + fn + '.csv', header=True, index=True)
 
 
 if __name__ == '__main__':
@@ -44,6 +53,9 @@ if __name__ == '__main__':
         '../samples/result/LSVT_feature_names(126x310)'
     ]
 
-    fn = 'all_acc_table.csv'
+    output_path = './acc_result/'
+    new_folder_path = [output_path + '/' + p.split('result')[-1] for p in path_list]
 
-    constate_acc_table(path_list, fn)
+    fn = 'all_acc_table'
+
+    constate_acc_table(path_list, new_folder_path, fn)
